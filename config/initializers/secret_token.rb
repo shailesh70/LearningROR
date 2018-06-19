63e532ea3085af66e3868eda14fab253bdceab6d
Shailesh Kumar <shailesh.kumar@freshworks.com>
2018-06-19 19:09:46 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlaBlaCar::Application.config.secret_token = '9c148488f335709e2c86ebcc40130527e736635ba7dc0ed8b265d8652abffb01a1dfefe225a7028d00eeb4f3239b9f831b8a7e098a249d8ac50d3e441687d7cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BlaBlaCar::Application.config.session_store :cookie_store, key: '_BlaBlaCar_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BlaBlaCar::Application.config.session_store :active_record_store
